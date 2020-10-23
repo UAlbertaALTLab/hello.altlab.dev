1a6e84fe686e917764e1e39a337966d7ab4a26f6
Serena Zhao <szhao5@ualberta.ca>
2020-10-23 02:06:18 -0600

    gunicorn config

--- app/app.py
+++ app/app.py
@@ -9,3 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "hello, world!"
+
+app.config['DEBUG'] = True
+
+if __name__ == "__main__":
+    # use 0.0.0.0 to use it in container
+    app.run(host='0.0.0.0')
